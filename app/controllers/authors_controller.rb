29897f01dbf87914532b3a07ed27d8be0d4e8c4b
AHMED AFANDY <eng.computer1@yahoo.com>
2020-08-19 20:15:37 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,6 +8,7 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
+    binding.pry
     if @author.valid?
        @author.save
       redirect_to author_path(@author)
